07a6c9116d26dba739c312e69e1262f518ba467a
Alex Hedley <alex.hedley@me.com>
2024-10-26 15:51:05 +0100

    WIP Score Creator

---
+++ src/components/score-creator/score-creator.component.js
@@ -0,0 +1,48 @@
+function ScoreCreatorController(scoreService) {
+    var ctrl = this;
+
+    this.$onInit = function () {
+        // console.log("oninit");
+    };
+
+    this.$onChanges = function (changesObj) {
+        console.log("onchanges");
+
+        console.log(changesObj);
+
+        if (changesObj.score.currentValue) {
+          ctrl.one_one_three = scoreService.oneOneThree(this.score);
+        }
+    };
+
+    function show() {
+        alert(this.score.date);
+    }
+
+    this.show = show;
+    this.getScores = getScores;
+    this.copyScores = copyScores;
+    this.deleteScores = deleteScores;
+
+    function getScores() {
+        scores = JSON.stringify(this.score);
+        $("#Scores").html(scores);
+    }
+
+    function copyScores() {
+        var scores = $("#Scores").html();
+        navigator.clipboard.writeText(scores);
+    }
+
+    function deleteScores() {
+        this.score = {};
+    }
+}
+
+angular.module("app").component("scoreCreator", {
+    templateUrl: "components/score-creator/score-creator.html",
+    controller: ScoreCreatorController,
+    bindings: {
+        score: "<",
+    },
+});
